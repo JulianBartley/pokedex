3993bd28c3a75d68adefaf8b90b4656994101fdd
JulianBartley <juliangbartley@gmail.com>
2019-11-17 14:37:20 -0500

    Welcome page and Oak page complete

---
+++ main.js
@@ -0,0 +1,40 @@
+
+//
+// function professorOakPage() {
+//    var pokedex = document.body;
+//    dundy.classList.toggle(".project");
+// }
+var page = document.getElementById('project');
+var icon = document.getElementById('pokemonClick')
+
+function professorOakPage(){
+  if (project.style.display === 'none'){
+    project.style.display = "block";
+    icon.style.display = 'none';
+  }else {
+    project.style.display = 'none';
+  }
+}
+
+
+
+
+
+
+
+
+
+//
+// function myFunction() {
+//    var dundy = document.body;
+//    dundy.classList.toggle("night-time");
+// }
+//
+//
+// <li><button class="button" onclick="myFunction()">DUNDIES</button>
+//
+//
+// .night-time{
+//   background-image: url("https://i.redd.it/rbgnajmyts311.jpg");
+//   color: white;
+// }
