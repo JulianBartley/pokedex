38758c13083cb28d1de9aae143bd434702fbfdd3
Julian Bartley <jbartley@nyc.yearup.org>
2019-11-18 14:46:16 -0500

    Added click feature to yes/no buttons

--- main.js
+++ main.js
@@ -10,6 +10,20 @@ function professorOakPage(){
   }
 }
 
+var goingForward = document.getElementById('yes');
+var name = document.getElementById('trainerName')
+
+function enterTrainer(){
+  if (yes.style.display === 'block'){
+    yes.style.display = "none";
+    trainerName.style.display = 'block';
+  }else {
+    yes.style.display = 'block';
+  }
+}
+
+let playerParty = document.getElementById('player-party')
+let generateBtn = document.getElementById('generate-btn')
 
 class Pokemon{
   constructor(name){
